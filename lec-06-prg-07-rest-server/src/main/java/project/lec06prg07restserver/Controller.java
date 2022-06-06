9ea78110c68652c1a7101979692e8279cfeaf5d7
Jieboong <jieboong82@daum.net>
2022-06-06 17:28:49 +0900

    migrate HashMap to JSON

--- lec-06-prg-07-rest-server/src/main/java/project/lec06prg07restserver/Controller.java
+++ lec-06-prg-07-rest-server/src/main/java/project/lec06prg07restserver/Controller.java
@@ -1,5 +1,6 @@
 package project.lec06prg07restserver;
 
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -10,22 +11,22 @@ public class Controller {
     private MembershipManager myManager = new MembershipManager();
 
     @PutMapping("/{memberId}")
-    public HashMap<String, String> put(@PathVariable String memberId, @RequestBody HashMap<String, String> form) {
-        return myManager.create(memberId, form.get(memberId));
+    public JSONObject put(@PathVariable String memberId, @RequestBody JSONObject form) {
+        return myManager.create(memberId, (String) form.get(memberId));
     }
 
     @GetMapping("/{memberId}")
-    public HashMap<String, String> get(@PathVariable String memberId) {
+    public JSONObject get(@PathVariable String memberId) {
         return myManager.read(memberId);
     }
 
     @PostMapping("/{memberId}")
-    public HashMap<String, String> post(@PathVariable String memberId, @RequestBody HashMap<String, String> form) {
-        return myManager.update(memberId, form.get(memberId));
+    public JSONObject post(@PathVariable String memberId, @RequestBody JSONObject form) {
+        return myManager.update(memberId, (String) form.get(memberId));
     }
 
     @DeleteMapping("/{memberId}")
-    public HashMap<String, String> delete(@PathVariable String memberId) {
+    public JSONObject delete(@PathVariable String memberId) {
         return myManager.delete(memberId);
     }
 }
