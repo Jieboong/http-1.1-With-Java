790e7616797bb7abc3d9d1108b700f6719896032
Jieboong <jieboong82@daum.net>
2022-06-06 16:17:51 +0900

    update POST Request with OkHttp

--- lec-06-prg-02-http-web-client/src/main/java/MyHttpClient.java
+++ lec-06-prg-02-http-web-client/src/main/java/MyHttpClient.java
@@ -1,14 +1,12 @@
+import okhttp3.*;
+
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MyHttpClient {
-
     public static void main(String[] args) throws URISyntaxException {
         System.out.println("## HTTP client started.");
 
@@ -20,7 +18,7 @@ public class MyHttpClient {
         try {
             String response = httpClient.sendAsync(
                     HttpRequest.newBuilder(
-                            new URI("http://localhost:9999/temp/"))
+                                    new URI("http://localhost:9999/temp/"))
                             .GET().build(), HttpResponse.BodyHandlers.ofString()
             ).thenApply(HttpResponse::body).get();
             System.out.println("## GET response [start]");
@@ -47,24 +45,26 @@ public class MyHttpClient {
         }
 
         System.out.println("## POST request for http://localhost:9999/ with var1 is 9 and var2 is 9");
-        String params = "?var1=9&var2=9";
+        String params = "var1=9&var2=9";
         try {
-            String uri = "http://localhost:9999/";
-
-            String response = httpClient.sendAsync(
-                    HttpRequest.newBuilder(
-                            new URI(uri)
-                    ).POST(HttpRequest.BodyPublishers.ofString(params)).build(), HttpResponse.BodyHandlers.ofString()
-            ).thenApply(HttpResponse::body).get();
             System.out.println("## POST response [start]");
-            System.out.println("response = " + response);
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("text/plain");
+            RequestBody body = RequestBody.create(mediaType, "var1=9&var2=9");
+            Request request = new Request.Builder()
+                    .url("http://localhost:9999/")
+                    .method("POST", body)
+                    .addHeader("Content-Type", "text/plain")
+                    .build();
+            Response response = client.newCall(request).execute();
+            System.out.println("response = " + response.body().string());
             System.out.println("## POST response [end]");
 
         } catch (Exception e) {
+            e.printStackTrace();
             System.out.println("Wrong Request");
-
         }
+        System.exit(0);
     }
-
-
 }
